4dd845a8950c229f064cf25c5dee062f8292377b
Apoorva-Saxena-hub <saxenapoorva1307@gmail.com>
2025-08-16 17:25:25 +0530

    creating custom widgets

--- lib/screens/create_room_screen.dart
+++ lib/screens/create_room_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/widgets/custom_text.dart';
+import 'package:tic_tac_toe/widgets/custom_textfield.dart';
 
 class CreateRoomScreen extends StatefulWidget {
   static String routeName = '/create-room';
@@ -9,8 +11,38 @@ class CreateRoomScreen extends StatefulWidget {
 }
 
 class _CreateRoomScreenState extends State<CreateRoomScreen> {
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _nameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            CustomText(
+              shadows: [Shadow(blurRadius: 40, color: Colors.blue)],
+              text: 'Create Room',
+              fontSize: 70,
+            ),
+            SizedBox(height: size.height * 0.08),
+            CustomTextfield(
+              controller: _nameController,
+              hintText: 'Enter your nickname',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/widgets/custom_textfield.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/utils/colors.dart';
+
+class CustomTextfield extends StatelessWidget {
+  final TextEditingController controller;
+  final String hintText;
+  const CustomTextfield({
+    super.key,
+    required this.controller,
+    required this.hintText,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          fillColor: bgColor,
+          filled: true,
+          hintText: hintText,
+        ),
+      ),
+    );
+  }
+}
