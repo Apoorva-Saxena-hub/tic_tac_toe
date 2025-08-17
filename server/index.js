cb6f04cc52a9a682c350f96f39bbaf2d5660cc57
Apoorva-Saxena-hub <saxenapoorva1307@gmail.com>
2025-08-17 10:49:47 +0530

    changes in server

--- server/index.js
+++ server/index.js
@@ -2,4 +2,16 @@ const express = require('express');
 const http = require('http');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+var server = http.createServer(app);
+
+var io = require("socket.io")(server);
+
+
+//client -> middleware -> server
+//middleware
+app.use(express.json());
+
+server.listen(port, '0.0.0.0', () =>{
+    console.log(`Server started and running on port ${port}`);
+});
